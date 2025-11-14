c8189510c5b13dab7123c04f5c3eefe0335c2dad
vladpaun1 <vlad.paun06@gmail.com>
2025-11-14 23:04:49 +0100

    shadcn themeing boilerplate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider"
+import { ModeToggle } from "@/components/theme-toggle";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,19 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          // enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+
+          <div className="fixed bottom-4 right-4">
+            <ModeToggle />
+          </div>
+        </ThemeProvider>
+        
       </body>
     </html>
   );
